e568bad8844b2574ccb1a9c4f504fd30eb07ba77
karan1171 <karanm98033@gmail.com>
2024-10-08 07:13:38 +0530

    String Info

---
+++ string basic.js
@@ -0,0 +1,6 @@
+/* console.log("Hello, There"); double quotations
+   console.log('Hello, There'); single quotations
+   these both works.If missed one of the quotations then it won,t work
+   console.log("Hello, there123") - This also works it gets treated like a string
+
+   */
