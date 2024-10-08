ecd3fcf26c88d5db7d12ef576574b7bdc321d3a3
karan1171 <karanm98033@gmail.com>
2024-10-08 06:54:48 +0530

    operators

---
+++ Arithmetic operations.js
@@ -0,0 +1,20 @@
+/* Arithmetic operations
++ addition
+- subtraction
+* multiplication
+/ division
+*/
+
+/* comparison operators
+> greater than
+< less than
+== equal
+*/
+// console.log(2 + 2);
+// console.log(1 + 2 + 3 + 4 + 5);
+// console.log(20 - 18);
+// console.log(2 * 3);
+// console.log(8 / 1);
+// console.log(3 > 2);
+// console.log(2 > 3);
+// cosole.log(10 == 10);
