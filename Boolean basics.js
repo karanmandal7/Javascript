1e56f3cf8b793819a9adbb211132e471a38bbfbf
karan1171 <karanm98033@gmail.com>
2024-10-08 07:56:09 +0530

    Boolean basics.

---
+++ Boolean basics.js
@@ -0,0 +1,12 @@
+/*console.log(1 > 2);
+  console.log(1 == 2);
+  Boolean functions only in True and False.
+  var score = 100;
+  console.log(100 == "100"); Only value is considerate not the data type
+  two equals sign works without taking consideration the data type but with three equals sign data type comes into consideration
+  100 === "100" checks value as well as data type
+  console.log(1 != 1); //false as 1 = 1
+  console.log(1 !== "1");//true as 1 in double quotations is string.
+  */
+
+   
