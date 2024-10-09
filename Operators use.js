9d47bdab4684d8e326829c05498878220bc7ee84
karan1171 <karanm98033@gmail.com>
2024-10-09 07:51:32 +0530

    Operators Use

---
+++ Operators use.js
@@ -0,0 +1,38 @@
+//var score = 8;
+//console.log("Mid-level skills:", 0 < 8 && 10 > 8); 
+//Using && Operator.
+
+//Using || Operator 
+
+//var timeRemaining = 0;
+//var energy = 10;
+//console.log("Game over: ", timeRemaining == 0 || energy == 0);
+
+// Using Modulus Operator (%)
+/*var num1 = 2;
+var num2 = 5;
+var test1 = num1 % 2;
+var test2 = num2  % 2;
+
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("Is", num2,"an even number?", result2 );
+*/
+
+//Using (+) Operator
+//var a = 5;
+//var b = 10;
+//console.log(a + b);
+
+// Using (+) operator
+//var now = "Now in ";
+//var three = 3;
+//var d = "D!";
+//console.log(now + three +  d);
+
+//USing (+=) Operator
+//var counter = 0;
+//counter += 5;
+//counter += 3;
+//console.log(counter);
