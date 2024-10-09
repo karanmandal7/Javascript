a104e5ee312d46778978bc64567a97bf2e902ed6
karan1171 <karanm98033@gmail.com>
2024-10-09 18:24:46 +0530

    LoopsTasks

---
+++ TasksWithLoops.js
@@ -0,0 +1,26 @@
+
+/*for(var i = 1; i <= 5; i++){
+    console.log(i);
+
+};
+console.log("Counting completed");
+*/
+/*
+for (var i = 5; i >= 0; i--){
+    console.log(i);
+};
+console.log("Complete");
+*/
+/*var i = 1;
+while(i<6){
+    console.log(i);
+    i++;   
+};
+console.log("Completed");
+*/
+
+var i = 2018;
+while(i < 2023){
+    console.log(i);
+    i++;
+};
