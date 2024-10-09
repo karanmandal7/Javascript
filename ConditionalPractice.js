8b5c93562553b0deac11678a8a682936cb6cefdb
karan1171 <karanm98033@gmail.com>
2024-10-09 10:52:54 +0530

    ConditionalPractice.

---
+++ ConditionalPractice.js
@@ -0,0 +1,41 @@
+/*var age = 18;
+if(age >= 65){
+    console.log("You get your income from your pension");
+}else if(age < 65 && age >= 18){
+    console.log("Each month you get a salary");
+
+    
+}else if(age < 18){
+    console.log("You get an allowance");
+}else{
+    console.log("The value of the age variable is not numerical");
+}
+*/ //if else.
+
+var day = "Sunday"
+switch(day){
+    case "Monday":
+        console.log("Do Something");
+        break;
+    case "Tuesday":
+        console.log("Do Something");
+        break;
+    case "Wednesday":
+        console.log("Do Something");
+        break;
+    case "Thrusday":
+        console.log("Do Something");
+        break;
+    case "Friday":
+        console.log("Do Something");
+        break;
+    case "Saturday":
+        console.log("Do Something");
+        break;
+    case "Sunday":
+        console.log("Die Today Please");
+        break;
+    default:
+        console.log("Do Nothing");                        
+
+}
