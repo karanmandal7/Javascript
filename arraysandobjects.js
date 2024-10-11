2f9a1f855c61fa094be92f6502e4c2f39c35bc81
karan1171 <karanm98033@gmail.com>
2024-10-11 17:03:02 +0530

    Push() & Pop(), Dot Notation method

---
+++ arraysandobjects.js
@@ -0,0 +1,20 @@
+
+var clothes = []; // push & pop method
+clothes.push("Shirt");
+clothes.push("pant");
+clothes.push("Tie");
+clothes.push("Underwear");
+clothes.push("socks");
+clothes.pop();
+clothes.push("Bow");
+console.log(clothes[4]);
+
+
+/*
+var favCar = {}; //dot notation
+favCar.color = "White";
+favCar.convertible = true;
+console.log(favCar);
+*/
+
+
