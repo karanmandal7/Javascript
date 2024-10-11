fa3504324569c1084a0a83cba2ca8828429844d2
karan1171 <karanm98033@gmail.com>
2024-10-11 18:32:06 +0530

    typeof operator to check the data type

---
+++ TypeOf.js
@@ -0,0 +1,11 @@
+//using typeof
+
+//var test = typeof('What is this');
+//var test = typeof(10);
+//var test = typeof(true);
+//var test = typeof(3.14);
+//var test = typeof(1 < 2);
+//var test = typeof([1,2,3]);
+//var test = typeof({ firstProperty : 1});
+var test = typeof(function abc(){ console.log('abc');});
+console.log(test);
