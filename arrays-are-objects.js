3c04672f4de220c9c14f1f39d9e42e2a2c901d5e
karan1171 <karanm98033@gmail.com>
2024-10-11 12:14:32 +0530

    Arrays are object Explanation

--- arrays-are-objects.js
+++ arrays-are-objects.js
@@ -16,19 +16,25 @@ function arrayBuilder(one, two, three){
     arr.push(one);
     arr.push(two);
     arr.push(three);
+   // console.log (arr);
     return arr;
     
 }
 /*
 I can now build a function that takes all its
 arguements and pushes them into a array like the above code.
-if i had console.log the array.Insted of return if i had console.log(arr).
+if i had console.log the array,insted of return arr; then, 
+the functio will return the arr insted of printing it to
+the console.
+The main difference is that you will need to either store
+the result in a variable or use console.log() when calling the 
+function to see the output.
 */
 /*
 Like in the above code code i returned it.
 Additionally, I can save this function call to a variable.
-Named simpleArr
-And now i can console the log values stored in simpleArr:
+Named simpleArr.
+And now i can console the log values stored in simpleArr:.
 */ 
 //arrayBuilder(1,2,3);//This calls the Function
  var simpleArr = arrayBuilder('apple','pear','plum');
