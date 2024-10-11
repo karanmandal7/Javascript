dd584cf6257c57e2dbe4c8c9f41467a40b5bb74a
karan1171 <karanm98033@gmail.com>
2024-10-11 13:26:02 +0530

    Math Random  method

---
+++ MathRandom.js
@@ -0,0 +1,7 @@
+Math.random();//Generate a decimal number between 0 and 0.99
+
+var decimal = Math.random();//save it to a variable:
+
+console.log(decimal);//Log the value of decimal to the console
+
+console.log(decimal * 10);//Log the value of decimal Multiplied by 10
