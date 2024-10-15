c94b14485d45cf3b2002749e53d5cf6722a09ca4
karan1171 <karanm98033@gmail.com>
2024-10-15 17:55:09 +0530

    oop - programming-play

---
+++ oop-programming.js
@@ -0,0 +1,26 @@
+class mobile{
+    useSystem(){
+        console.log("Smooth Running")
+    }
+}
+class keypad extends mobile{
+    useSystem(){
+        console.log("Kam Chalau")
+    }
+}
+class android extends mobile{
+    useSystem(){
+        console.log("Slow Functioning")
+    }
+}
+
+class os extends mobile{
+    useSystem(){
+        console.log("Fast & Furious")
+    }
+}
+var samsung = new android();
+var iphone = new os();
+samsung.useSystem();
+iphone.useSystem();
+    
