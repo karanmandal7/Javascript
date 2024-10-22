5be53e9fdc7ddb97aed8ccef604cf04e89a5de80
karan1171 <karanm98033@gmail.com>
2024-10-22 17:33:38 +0530

    Practice with conditionals & operators

---
+++ Practice_With_More_Understanding.js/randompractice2.js
@@ -0,0 +1,47 @@
+/*
+let age = 18
+
+if (age > 10 && age < 20) {
+  console.log("Your age lies between 10 and 20")
+}
+else {
+  console.log("Your age doesnt lies between 10 and 20")
+}
+*/
+
+// Problem No 2
+/*
+let age = 18
+switch (age) {
+  case '12':
+    console.log("Your age is 12")
+    break
+  case '13':
+    console.log("Your age is 13")
+    break
+  case '14':
+    console.log("Your age is 14")
+    break
+  case '15':
+    console.log("Your age is 15")
+    break
+  default:
+    console.log("Your age is not special")
+}
+*/
+// Problem No 3
+/*
+let num = 12
+
+if (num % 2 == 0 && num % 3 == 0) {
+  console.log("Your number is divisible by 2 and 3")
+}
+else {
+  console.log("Your number is not divisible by 2 and 3")
+}
+*/
+
+// Problem No 5
+let age2 = 19
+let a1 = age2 > 18 ? "You can drive" : "You cannot drive"
+console.log(a1)
