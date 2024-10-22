ccc47bcbc4c3905c6f61dbe83b2692620707fedc
karan1171 <karanm98033@gmail.com>
2024-10-22 17:47:36 +0530

    while_loops

---
+++ Practice_With_More_Understanding.js/while_loops.js
@@ -0,0 +1,17 @@
+let n = 10;
+let i = 0;
+while(i<n){
+  console.log(i)
+  i++;
+}
+/*
+// while loop
+let n1 = 20
+n = Number.parseInt(n)
+
+let j = 10;
+do{
+  console.log(j)
+  j++;
+}while (j < n)
+*/
