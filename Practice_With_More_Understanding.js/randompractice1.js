8f92fa5ef7a047943284e0b9b922447d5956cede
karan1171 <karanm98033@gmail.com>
2024-10-22 16:55:58 +0530

    random practice with variables

---
+++ Practice_With_More_Understanding.js/randompractice1.js
@@ -0,0 +1,32 @@
+//1.To add a string an number
+let a = "Harry"
+let b = 6
+console.log(a + b)
+
+
+console.log(typeof (a+b))//typeof to get the type of parameters
+
+//object play
+const a1 = {
+  name: "Harry",
+  section: 1,
+  isPrincipal: false
+}
+ //a1['section'] = 45 //like this we can manipulate const variables
+ //a1 = {} //a1 = {} TypeError: Assignment to constant variable.at Object.
+
+    
+
+// 
+//a1['friend'] = "Shubham"  
+a1['name'] = "Lovish"  
+console.log(a1)
+
+// Dictionary , different code
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
