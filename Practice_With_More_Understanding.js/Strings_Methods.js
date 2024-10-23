43269b3ad84c83903554fc42318a700df6c27570
karan1171 <karanm98033@gmail.com>
2024-10-23 11:55:31 +0530

    String Methods

---
+++ Practice_With_More_Understanding.js/Strings_Methods.js
@@ -0,0 +1,29 @@
+let name = "Karan"
+// console.log(name.length)//length of string
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+// Slice Method 
+// console.log(name.slice(2, 4))
+ //console.log(name.slice(3))
+
+ // Replace Method
+ //console.log(name.replace("Kar", "Mar"))
+
+ // concat method
+  let friend = "Salman"
+  console.log(name.concat(" is a friend of ", friend, " Ok"))
+
+ //Trim Method
+ // let friend2 = "      Meena        "
+// console.log(friend2)
+// console.log(friend2.trim())
+
+let fr = "Raman" +  "Bhoomi"  + "Karan"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+// fr[4]= "o" // This is not possible 
+console.log(fr)
