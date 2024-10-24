7f742d78bb38acaca6a40718bc34abb00e3bb87f
karan1171 <karanm98033@gmail.com>
2024-10-24 12:01:33 +0530

    alert_confirm__prompt

---
+++ Practice_With_More_Understanding.js/alert_prompt_confirm.js/script.js
@@ -0,0 +1,13 @@
+//const prompt = require('prompt-sync')(); // not for browser 
+alert("Enter the value of a!")
+let a = prompt("Enter a here", "578")
+a = Number.parseInt(a)
+alert("You entered a of type " + (typeof a))
+let write = confirm("Do you want to write it to the page")
+if (write) {
+  document.write(a)
+}
+else {
+  document.write("Please allow me to write")
+}
+
